8fdaf2b52fc30ad55dfc59580ebc828cf0678165
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-22 02:00:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,11 +5,12 @@ def directors_totals(nds)
 
   newHash = Hash.new
   row_index = 0
-  grandTotal = 0
+
   while row_index < directors_database.length do
     column_index = 0
+    grandTotal = 0
     while column_index < directors_database[row_index].length do
-      puts directors_database[row_index][:movies][column_index][:worldwide_gross]
+      grandTotal +=directors_database[row_index][:movies][column_index][:worldwide_gross]
       
       
       column_index += 1
