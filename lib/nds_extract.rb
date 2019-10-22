e8a4bd8538beef547e7861ebd97f9d392453ed06
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-22 02:11:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,13 +11,11 @@ def directors_totals(nds)
     grandTotal = 0
     while column_index < directors_database[row_index][:movies].length do
       grandTotal +=directors_database[row_index][:movies][column_index][:worldwide_gross]
-      puts "The grandTotal at the moment is #{grandTotal}"
       column_index += 1
     end
     
     director = directors_database[row_index][:name]
     newHash[director] = grandTotal
-    puts "The grand total of #{director} is #{grandTotal}"
     row_index += 1
   end
   return newHash
